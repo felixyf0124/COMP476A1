6c4cee905f9d5eed77b2da9fafbe05dcab310386
felixyf0124 <felixyf0124@gmail.com>
2020-01-28 17:23:53 -0500

    added kinematic flee & arrive function

--- comp476A1/Assets/Scenes/Script/MoveStrategy.cs
+++ comp476A1/Assets/Scenes/Script/MoveStrategy.cs
@@ -7,34 +7,52 @@ public class MoveStrategy : MonoBehaviour
     /**
      * return velocity
      */
-    public Vector3 seekKinematic(Vector3 pTar, Vector3 pCur, Vector3 vMax)
+    public Vector3 seekKinematic(Vector3 positionTarget, Vector3 positionCurrent, Vector3 velocityMax)
     {
 
-        Vector2 pT = new Vector2(pTar.x, pTar.z);
+        Vector2 pTar = new Vector2(positionTarget.x, positionTarget.z);
 
-        Vector2 pC = new Vector2(pCur.x, pCur.z);
+        Vector2 pCur = new Vector2(positionCurrent.x, positionCurrent.z);
 
-        Vector2 vM = new Vector2(vMax.x, vMax.z);
+        Vector2 vMax = new Vector2(velocityMax.x, velocityMax.z);
 
-        Vector2 vDir = pT - pC;
+        Vector2 vDir = pTar - pCur;
 
         //Normalnize
         vDir.Normalize();
 
-        Vector2 ksVelocity = vM * vDir;
+        Vector2 ksVelocity = vMax * vDir;
 
-        return (new Vector3(ksVelocity.x, 0.0f, ksVelocity.y));
+        return (new Vector3(ksVelocity.x, positionCurrent.y, ksVelocity.y));
     }
 
 
-    public void arriveKinematic(Vector3 pos, Vector3 tar)
+    public Vector3 arriveKinematic(Vector3 positionTarget, Vector3 positionCurrent, Vector3 velocityMax, float rSat)
     {
+        Vector2 pTar = new Vector2(positionTarget.x, positionTarget.z);
+
+        Vector2 pCur = new Vector2(positionCurrent.x, positionCurrent.z);
+
+        Vector2 vMax = new Vector2(velocityMax.x, velocityMax.z);
+
+        Vector2 vDir = pTar - pCur;
+
+        Vector2 ksVelocity = new Vector2(0.0f,0.0f);
+
+        if (vDir.magnitude > rSat)
+        {
+            vDir.Normalize();
+
+            ksVelocity = vMax * vDir;
+        }
+
+        return (new Vector3(ksVelocity.x, positionCurrent.y, ksVelocity.y));
 
     }
 
 
-    public void fleeKinematic()
+    public Vector3 fleeKinematic(Vector3 pTar, Vector3 pCur, Vector3 vMax)
     {
-
+        return -seekKinematic(pTar, pCur, vMax);
     }
 }
